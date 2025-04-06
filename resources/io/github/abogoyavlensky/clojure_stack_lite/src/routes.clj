e0cb151b32bc4e1be009edf379514c16c6612ad1
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-04-07 00:36:40 +0100

    Fix css build and add health endpoint

--- resources/io/github/abogoyavlensky/clojure_stack_lite/src/routes.clj
+++ resources/io/github/abogoyavlensky/clojure_stack_lite/src/routes.clj
@@ -1,7 +1,10 @@
 (ns {{main/ns}}.routes
-  (:require [{{main/ns}}.handlers :as handlers]))
+  (:require [ring.util.response :as response]
+            [{{main/ns}}.handlers :as handlers]))
 
 (def routes
   [["/" {:name ::home-page
          :get {:handler handlers/home-handler}
-         :responses {200 {:body string?}}}]])
+         :responses {200 {:body string?}}}]
+   ["/health" {:name ::health-check
+               :get {:handler (fn [_] (response/response "OK"))}}]])
