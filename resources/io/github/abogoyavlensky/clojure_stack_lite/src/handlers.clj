598ce04eb9da092d9ad1510a3e08ddf27dc6cc74
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-04-06 23:05:03 +0100

    Move home handler

--- resources/io/github/abogoyavlensky/clojure_stack_lite/src/handlers.clj
+++ resources/io/github/abogoyavlensky/clojure_stack_lite/src/handlers.clj
@@ -3,13 +3,13 @@
             [ring.util.response :as response]
             [{{main/ns}}.views :as views]))
 
-(defn home-handler
-  [_]
-  (reitit-extras/render-html views/home-page))
-
 (defn default-handler
   [error-text status-code]
   (fn [_]
     (-> (views/error-page error-text)
         (reitit-extras/render-html)
         (response/status status-code))))
+
+(defn home-handler
+  [_]
+  (reitit-extras/render-html views/home-page))
