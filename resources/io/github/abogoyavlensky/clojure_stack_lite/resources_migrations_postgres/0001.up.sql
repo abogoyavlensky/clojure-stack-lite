7d76b51ab893f8ee884fe6763fc59464e905641f
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2025-11-15 15:54:35 +0200

    more fixes and better structure

---
+++ resources/io/github/abogoyavlensky/clojure_stack_lite/resources_migrations_postgres/0001.up.sql
@@ -0,0 +1,2 @@
+-- Create extension for case-insensitive text type
+CREATE EXTENSION IF NOT EXISTS citext;
--- resources/io/github/abogoyavlensky/clojure_stack_lite/resources_migrations_postgres_auth/0002.up.sql
+++ resources/io/github/abogoyavlensky/clojure_stack_lite/resources_migrations_postgres_auth/0002.up.sql
@@ -1,6 +1,3 @@
--- Create extension for case-insensitive text type
-CREATE EXTENSION IF NOT EXISTS citext;
-
 -- Create users table with authentication-related fields
 CREATE TABLE IF NOT EXISTS "user" (
     id SERIAL PRIMARY KEY,
